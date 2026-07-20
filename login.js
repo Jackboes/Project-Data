0f862128843ea925b856167c350ed0f913e18eea
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-07-20 12:49:30 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,3 +5,30 @@ document.querySelector(".btn").addEventListener("click",function(){
     signAudio.currentTime = 0 
     signAudio.play()
 });
+
+
+
+var validatePassword = document.querySelector(".form-control-two");
+
+validatePassword.addEventListener("blur", function(){
+    if(validatePassword.value.length != 8){
+        alert("Password must be of 8 characters");
+    }
+
+    else{
+        alert("Valid Enter");
+    }
+})
+
+const password = document.querySelector(".pass-value");
+const showPassword = document.querySelector(".show-pass");
+
+showPassword.addEventListener("click",function(){
+// Here checked is a built-in js method which can be used whenever you use a check box it has true and false value.
+    if(showPassword.checked){
+        password.setAttribute("type","text");
+    }
+    else{
+        password.setAttribute("type","password");
+    }
+});
