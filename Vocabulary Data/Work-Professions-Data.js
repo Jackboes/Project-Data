6483d1e3a6c532762244ed550d511666b5eb46e5
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-08-23 20:45:06 +0530

    Add work professions vocabulary data

---
+++ Vocabulary Data/Work-Professions-Data.js
@@ -0,0 +1,64 @@
+const workProfessions = [
+    {
+        word: "die Arbeit",
+        meaning: "Work / Job"
+    },
+    {
+        word: "der Beruf",
+        meaning: "Profession / Occupation"
+    },
+    {
+        word: "das Büro",
+        meaning: "Office"
+    },
+    {
+        word: "der Chef",
+        meaning: "Boss (Male)"
+    },
+    {
+        word: "die Chefin",
+        meaning: "Boss (Female)"
+    },
+    {
+        word: "der Kollege",
+        meaning: "Colleague (Male)"
+    },
+    {
+        word: "die Kollegin",
+        meaning: "Colleague (Female)"
+    },
+    {
+        word: "der Computer",
+        meaning: "Computer"
+    },
+    {
+        word: "die E-Mail",
+        meaning: "Email"
+    },
+    {
+        word: "das Formular",
+        meaning: "Form"
+    },
+    {
+        word: "der Arzt",
+        meaning: "Doctor (Male)"
+    },
+    {
+        word: "die Ärztin",
+        meaning: "Doctor (Female)"
+    },
+    {
+        word: "der Lehrer",
+        meaning: "Teacher (Male)"
+    },
+    {
+        word: "die Lehrerin",
+        meaning: "Teacher (Female)"
+    },
+    {
+        word: "arbeiten",
+        meaning: "To work"
+    }
+];
+
+export default workProfessions;
