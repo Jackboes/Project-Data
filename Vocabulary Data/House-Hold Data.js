c3c3b7495dc0ead9b23e6ec94f52233eb568b750
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-08-23 20:40:05 +0530

    Add household vocabulary data in House-Hold Data.js

---
+++ Vocabulary Data/House-Hold Data.js
@@ -0,0 +1,64 @@
+const homeHold = [
+    {
+        word: "das Haus",
+        meaning: "House"
+    },
+    {
+        word: "die Wohnung",
+        meaning: "Apartment / Flat"
+    },
+    {
+        word: "die Küche",
+        meaning: "Kitchen"
+    },
+    {
+        word: "das Bad",
+        meaning: "Bathroom"
+    },
+    {
+        word: "das Schlafzimmer",
+        meaning: "Bedroom"
+    },
+    {
+        word: "das Wohnzimmer",
+        meaning: "Living room"
+    },
+    {
+        word: "der Tisch",
+        meaning: "Table"
+    },
+    {
+        word: "der Stuhl",
+        meaning: "Chair"
+    },
+    {
+        word: "das Bett",
+        meaning: "Bed"
+    },
+    {
+        word: "der Schrank",
+        meaning: "Cupboard / Wardrobe"
+    },
+    {
+        word: "der Kühlschrank",
+        meaning: "Refrigerator"
+    },
+    {
+        word: "die Lampe",
+        meaning: "Lamp"
+    },
+    {
+        word: "die Tür",
+        meaning: "Door"
+    },
+    {
+        word: "das Fenster",
+        meaning: "Window"
+    },
+    {
+        word: "der Schlüssel",
+        meaning: "Key"
+    }
+];
+
+export default homeHold;
