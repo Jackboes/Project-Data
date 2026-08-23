3584b92a3f99a51e82a4c58b74f25f385ae190d8
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-08-23 20:40:42 +0530

    Add personal information vocabulary data

---
+++ Vocabulary Data/Personal-Information-Data.js
@@ -0,0 +1,80 @@
+const personalInfo = [
+
+    {
+        word: "Name",
+        meaning: "Name"
+    },
+
+    {
+        word: "Vorname",
+        meaning: "First name"
+    },
+
+    {
+        word: "Nachname",
+        meaning: "Last name / Surname"
+    },
+
+    {
+        word: "Alter",
+        meaning: "Age"
+    },
+
+    {
+        word: "Adresse",
+        meaning: "Address"
+    },
+
+    {
+        word: "Straße",
+        meaning: "Street"
+    },
+
+    {
+        word: "Hausnummer",
+        meaning: "House number"
+    },
+
+    {
+        word: "Telefonnummer",
+        meaning: "Telephone number"
+    },
+
+    {
+        word: "E-Mail-Adresse",
+        meaning: "Email address"
+    },
+
+    {
+        word: "Land",
+        meaning: "Country"
+    },
+
+    {
+        word: "Nationalität",
+        meaning: "Nationality"
+    },
+
+    {
+        word: "Geburtsort",
+        meaning: "Place of birth"
+    },
+
+    {
+        word: "Geburtsdatum",
+        meaning: "Date of birth"
+    },
+
+    {
+        word: "Wohnort",
+        meaning: "Place of residence"
+    },
+
+    {
+        word: "Stadt",
+        meaning: "City"
+    }
+
+];
+
+export default personalInfo;
