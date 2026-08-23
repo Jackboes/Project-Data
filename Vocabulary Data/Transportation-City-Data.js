ef289b2c43788995ef86a7de7f7faae3e6b15b66
Arinjay <92075314+Jackboes@users.noreply.github.com>
2026-08-23 20:44:30 +0530

    Add transportation city vocabulary data

---
+++ Vocabulary Data/Transportation-City-Data.js
@@ -0,0 +1,63 @@
+const transportCity = [
+    {
+        word: "die Stadt",
+        meaning: "City"
+    },
+    {
+        word: "die Straße",
+        meaning: "Street / Road"
+    },
+    {
+        word: "der Platz",
+        meaning: "Square / Plaza"
+    },
+    {
+        word: "die Ampel",
+        meaning: "Traffic light"
+    },
+    {
+        word: "die Brücke",
+        meaning: "Bridge"
+    },
+    {
+        word: "das Auto",
+        meaning: "Car"
+    },
+    {
+        word: "das Fahrrad",
+        meaning: "Bicycle"
+    },
+    {
+        word: "die S-Bahn",
+        meaning: "Suburban train / Commuter rail"
+    },
+    {
+        word: "die U-Bahn",
+        meaning: "Subway / Underground"
+    },
+    {
+        word: "die Haltestelle",
+        meaning: "Bus/Tram stop"
+    },
+    {
+        word: "links",
+        meaning: "Left"
+    },
+    {
+        word: "rechts",
+        meaning: "Right"
+    },
+    {
+        word: "geradeaus",
+        meaning: "Straight ahead"
+    },
+    {
+        word: "die Karte",
+        meaning: "Map"
+    },
+    {
+        word: "der Weg",
+        meaning: "Path / Way"
+    }
+];
+export default transportCity;
